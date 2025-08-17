0dd0920f293c9dfe5da91c65126d27574ec8fa5c
Hector <hector.castorena.castillogmail.com>
2025-08-16 22:25:07 -0600

    config serve-static and initialize pokemon module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveStaticOptions: {
+        fallthrough: false,
+      },
+    }),
+    PokemonModule
+  ],
   controllers: [],
   providers: [],
 })
