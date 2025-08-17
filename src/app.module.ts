814991a0c03c330f7703e3eecad16862c2ba73ac
Hector <hector.castorena.castillogmail.com>
2025-08-16 22:51:40 -0600

    config connection with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -11,6 +12,9 @@ import { PokemonModule } from './pokemon/pokemon.module';
         fallthrough: false,
       },
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule
   ],
   controllers: [],
